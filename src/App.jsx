c8090150b279f37e901ab88bb2428ba6d2cceab9
SARTHAK MISHRA <147376925+Sarthakmishra001@users.noreply.github.com>
2024-09-19 13:10:22 -0500

    Updated the project with new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 const App = ()=> {
   return (
     <div>
-      <h1>hello world</h1>
+      <div className = "heading" >
+      <h1>Sheryians</h1>
+      <h1>Coding</h1>
+      <h1>School</h1>
+      </div>
     </div>
   )
 }
